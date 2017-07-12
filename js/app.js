e6ca7764020f5477de0a21759903157e25481e02
Luz Gutierrez <mili01gm@gmail.com>
2017-07-12 02:25:20 -0500

    Listo estilos. Medidas de skills agregadas. Español casi listo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 $(_ => {
-    $('.button-collapse').sideNav();
     console.log('Hola! Gracias por revisar mi código. Mi portafolio está en desarrollo y mis proyectos personales los iré agregando poco a poco. Para contacto o feedback escríbeme a mili01gm@gmail.com');
 
     $(window).on('scroll', () => {
