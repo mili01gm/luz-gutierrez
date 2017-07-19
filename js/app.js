517f98b805f2d693b22ee858ec7129c4f64fc52f
Luz Gutierrez <mili01gm@gmail.com>
2017-07-19 09:21:01 -0500

    arreglos margenes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 $(_ => {
-    $('.parallax').parallax();
     console.log('Hola! Gracias por revisar mi código. Mi portafolio está en desarrollo y mis proyectos personales los iré agregando poco a poco. Para contacto o feedback escríbeme a mili01gm@gmail.com');
 
     $(window).on('scroll', () => {
@@ -10,4 +9,15 @@ $(_ => {
             $('nav').removeClass('bg-darker');
         }
     });
+
+    $(window).bind("resize", function() {
+        console.log($(this).width())
+        if ($(this).width() < 500) {
+            $('.text-home-col').removeClass('flex');
+        } else {
+            $('.text-home-col').addClass('flex');
+        }
+    });
+
+
 });
